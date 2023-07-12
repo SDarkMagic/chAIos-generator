e2ff4d8bd4c6d746d75b62fb4068adbde1422022
SDarkMagic <dragonbuilderx@gmail.com>
2023-07-12 16:45:34 -0400

    Update bfres lib path reference to be dynamic. Add support for newer version of msbt files introduced with totk

---
+++ src/totk.py
@@ -0,0 +1,49 @@
+# Functions and methods for handling the msbt files found in the tears of the kingdom data
+import util
+import io
+import sys
+import time
+import numpy as np
+import pathlib
+import os
+
+msbt_lib_path = pathlib.Path(__file__).parent / 'lib/EPD-Libraries/msbt/build/pymsbt3/'
+sys.path.append(str(msbt_lib_path.absolute()))
+import pymsbt3 as msbt
+
+class MSBT:
+    def __init__(self, msbt_path) -> None:
+        self.path = pathlib.Path(msbt_path)
+        self.data = msbt.FromBinary(list(np.fromfile(str(self.path.absolute()), dtype='uint8')))
+
+    def replace_strings(self, replacement_method, **kwargs):
+        start_time = time.time()
+        entries = self.data.text_section.text_entries
+        for entry in entries:
+            current_msg = ""
+            for segment in entry.values:
+                current_msg += f' | {segment.text}'
+                self.data.text_section.text_entries[entries.index(entry)].values[entry.values.index(segment)].text = replacement_method(segment.text, **kwargs)
+
+        function_time = time.time() - start_time
+        print(f'Function completed in {function_time} seconds.')
+        return
+
+    def save(self):
+        buf = io.BytesIO()
+        raw = bytearray(self.data.ToBinary())
+        print('Finished converting to binary')
+        print(raw)
+        with open(str(self.path.absolute()), 'wb') as output:
+            output.write(raw)
+
+def pass_val(data):
+    #print(f"called pass_val function with value {data}")
+    return "test string"
+
+if __name__ == '__main__':
+    msg = MSBT("C:/Users/drago/Documents/totkMods/Text/romfs/Mals/USen.Product.100/ActorMsg/PouchContent.msbt")
+    data = msg.data
+    msg.replace_strings(pass_val)
+    msg.save()
+    print(data.ToText())
